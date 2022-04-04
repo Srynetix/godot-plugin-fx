4fa6368b134ff3dccc9a89422674f7a9aacb4104
Denis BOURGE <bourge.denis@gmail.com>
2022-04-04 02:03:46 +0200

    feat: tween method

--- screen-effects/FXCamera.cs
+++ screen-effects/FXCamera.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Godot;
 
 public class FXCamera : Camera2D
@@ -6,6 +7,13 @@ public class FXCamera : Camera2D
     public int MaxShakeStrength = 2;
     [Export]
     public float ShakeRatio = 0;
+
+    private Tween Tween;
+
+    public override void _Ready()
+    {
+        Tween = GetNode<Tween>("Tween");
+    }
 
     public override void _Process(float delta)
     {
@@ -16,5 +24,14 @@ public class FXCamera : Camera2D
     {
         float coef = (float)GD.RandRange(-1.0f, 1.0f) * MaxShakeStrength * ShakeRatio;
         Offset = new Vector2(coef, coef);
+    }
+
+    async public Task TweenToPosition(Vector2 position, float speed = 0.5f, float zoom = 1) {
+        Tween.StopAll();
+        Tween.InterpolateProperty(this, "global_position", GlobalPosition, position, speed, Tween.TransitionType.Quad, Tween.EaseType.InOut);
+        Tween.InterpolateProperty(this, "zoom", Zoom, Vector2.One * zoom, speed, Tween.TransitionType.Quad, Tween.EaseType.InOut);
+        Tween.Start();
+
+        await ToSignal(Tween, "tween_all_completed");
     }
 }
